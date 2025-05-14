2880db9ff0576e844b127dd9fc6d9e4cb63ae463
ChenYeh_Chien <chenyeh_chien@pegatroncorp.com>
2025-05-14 11:10:17 +0800

    complete UI

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,10 +1,28 @@
 import './Card.css';
 
 function Card() {
+  const name = "Sarah Dole";
+  const socialAt = "@sarahdole"
+  const content = 
+    "I've been searching for high-quality abstract images for my design projects," +
+    "and I'm trilled to have found this platform. " + 
+    "The variety and depth of creativity if astounding!"
+
   return (
     <div className='card'>
         <div className='card__info'>
-            123
+          <img src="./src/assets/profile-thumbnail.png" />
+          <div className='card__info__name'>
+            <div className='card__info__name--text'>
+              <span>{ name }</span>
+            </div>
+            <div className='card__info__name--at'>
+              <span>{ socialAt }</span>
+            </div>
+          </div>
+        </div>
+        <div className='card__content'>
+          { content }
         </div>
     </div>
   )
